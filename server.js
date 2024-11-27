350d20bbf231e364c809c1dbd68c9961e1585b18
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2024-11-27 17:03:24 -0300

    Front

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/userRoutes.js";
+import cors from 'cors'; // Importa o pacote CORS
 
 const app = express();
+
+// Configura o middleware CORS
+app.use(cors({
+    origin: 'http://localhost:5173', // URL do seu front-end (ajuste conforme necessário)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+}));
+
+
 app.use(express.json());
 
-const port = 3000;
+const port = 8080;
 
 mongoose.connect('mongodb+srv://Erick:123@cluster0.thocx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log("Conectado ao MongoDB"))
